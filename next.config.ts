4e972b6e13ed09176b34851925e1970ff7252318
Uwazzi | From Post-it to Prototype <97283772+uwazzi@users.noreply.github.com>
2025-05-14 19:01:41 -0500

    vercel config cleaning

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'], // Add any image domains you need to use
